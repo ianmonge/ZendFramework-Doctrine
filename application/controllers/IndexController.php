31c25b5ed7dcce2eaf8fc7b497b8e3b9f4e0a924
Ian Monge <ianmonge@gmail.com>
2011-09-11 11:51:42 +0200

    Select a single row, and updated it

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -2,16 +2,6 @@
 
 class IndexController extends Zend_Controller_Action
 {
-//    public function init()
-//    {
-//        /* Initialize action controller here */
-//    }
-//
-//    public function indexAction()
-//    {
-//        // action body
-//    }
-
     public function init()
     {
         $registry = Zend_Registry::getInstance();
@@ -20,12 +10,41 @@ class IndexController extends Zend_Controller_Action
  
     public function indexAction()
     {
+    }
+
+    public function insertAction()
+    {
+        $this->render('index');
+
         $projectEntity = new Default_Model_Project;
         $projectEntity->setName('Zaphod Beeblebrox');
         $this->_em->persist($projectEntity);
         $this->_em->flush();
     }
 
+    public function singleAction()
+    {
+        $this->render('index');
 
+        $q = $this->_em->createQuery('select p from Default_Model_Project p where p.name = ?1');
+        $q->setParameter( 1, 'Mercafutbol' );
+        $projectEntity = $q->getSingleResult();
+
+        var_dump( $projectEntity );
+    }
+
+    public function updateAction()
+    {
+        $this->render('index');
+
+        $q = $this->_em->createQuery('select p from Default_Model_Project p where p.name = ?1');
+        $q->setParameter( 1, 'Mercafutbol updated' );
+        $projectEntity = $q->getSingleResult();
+//        $projectEntity->setName('Mercafutbol');
+//        $this->_em->persist($projectEntity);
+//        $this->_em->flush();
+
+        var_dump( $projectEntity );
+    }
 }
 
